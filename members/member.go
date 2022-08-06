46943d51bc046722ff824081d124b0174bd2db9e
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-06 17:08:51 +0700

    fix register endpoint issue

--- members/member.go
+++ members/member.go
@@ -3,7 +3,7 @@ package members
 import "time"
 
 type Member struct {
-	MemberId  int       `json:"member_id" gorm:"column:member_id"`
+	ID        int       `json:"member_id" gorm:"column:member_id"`
 	Email     string    `json:"email" gorm:"column:email"`
 	Password  string    `json:"password" gorm:"column:password"`
 	Role      string    `json:"role" gorm:"column:role"`
